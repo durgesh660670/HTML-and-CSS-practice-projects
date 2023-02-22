0cd640dddb9ae090fb0eaaeedc7c5414d1d730e5
Durgesh <ddgupta000@gmail.com>
2023-02-22 11:10:38 +0530

    22feb2023

--- toDo/script.js
+++ toDo/script.js
@@ -1,15 +1,33 @@
 var itemsArray = localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")) : []
 
-// console.log(itemsArray)
+console.log(itemsArray)
+
+document.querySelector('#submit').addEventListener('click', () => {
+
+    var item = document.getElementById('item')
+    createItem(item);
+})
+
+var hideNewToTOBox = function () {
+    var newTB = document.querySelector('.new-to-do-box');
+    newTB.style.display = 'none'
+
+}
 
 document.querySelector('#enter').addEventListener('click', () => {
 
-    var item = document.querySelector('#item');
-    createItem(item);
+
+    var toDoList = document.querySelector('.to-do-list');
+    toDoList.style.display = 'none'
+    var newTB = document.querySelector('.new-to-do-box');
+    newTB.style.display = 'block'
+
+
 })
 
 
 var createItem = function (item) {
+
 
     itemsArray.push(item.value)
 
@@ -33,11 +51,14 @@ var displayItems = function () {
             <i class="fa-sharp fa-solid fa-trash  deleteBtn"></i>
            
             <i class="fa-solid fa-pen-to-square editBtn"></i>
+            <i class="fa-regular fa-star"></i>
             </div>
+            
         </div>
         <div class="save-div">
 
             <button class="saveBtn">Save</button>
+            <small>Edited</small>
            
         </div>
     </div>`
@@ -82,6 +103,7 @@ var editListeners = function () {
     editBtnObj.forEach((e, i) => {
 
         e.addEventListener('click', () => {
+
             saveDivObj[i].style.display = 'block'
             inputsObj[i].disabled = false
         })
@@ -125,8 +147,80 @@ function displayDate() {
 
 
 
+var search = function (e) {
+    var key = e.keyCode;
+    if (key == 13) {
+
+        var searchObj = document.getElementById('search').value;
+        // console.log(searchObj)
+        // console.log(searchObj)
+        if (searchObj && searchObj.length != 0) {
+            var itemsIndexResult = seachingAlgo(searchObj, itemsArray)
+            console.log(itemsIndexResult)
+            if (itemsIndexResult) {
+
+                var items = '';
+
+                for (var i in itemsIndexResult) {
+
+                    items += ` <div class="item">
+                    <div class="input-controller">
+            
+                        <textarea name="" id="" cols="30" rows="10" disabled>${itemsArray[itemsIndexResult[i]]}</textarea>
+                        <div class="action-div">
+                        <i class="fa-sharp fa-solid fa-trash  deleteBtn"></i>
+                       
+                        <i class="fa-solid fa-pen-to-square editBtn"></i>
+                        <i class="fa-regular fa-star"></i>
+                        </div>
+                        
+                    </div>
+                    <div class="save-div">
+            
+                        <button class="saveBtn">Save</button>
+                        <small>Edited</small>
+                       
+                    </div>
+                </div>`
+
+                }
+
+                document.querySelector('.to-do-list').innerHTML = items;
+            }
+            else {
+                console.log(itemsIndexResult)
+            }
+        }
+        else{
+            console.log(searchObj)
+        }
+        deleteListeners()
+        editListeners()
+        saveListeners()
+    }
+
+
+}
+
+
+var seachingAlgo = function (searchObj, itemsArray) {
+
+    // console.log(searchObj)
+    // console.log(itemsArray)
+    var itemsIndex = []
+
+    for (var i = 0; i < itemsArray.length; i++) {
+        if (itemsArray[i].includes(searchObj))
+            itemsIndex.push(i)
+    }
+
+    return itemsIndex;
+}
+
+
 
 window.addEventListener('load', () => {
     displayItems()
+    hideNewToTOBox()
     // displayDate()
 })
